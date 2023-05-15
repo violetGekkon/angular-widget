63cb8edba989c54beee7fc523d3fcd3c497a18c9
Anastasia <violetGekkon@gmail.com>
2023-05-15 17:57:54 +0300

    feat(lib*interfaces): subdivision interface added

---
+++ libs/shared/data-access/interfaces/src/lib/entities/subdivision.interface.ts
@@ -0,0 +1,11 @@
+export interface Subdivision {
+  id: string;
+  ownerID: string;
+  fullName: string;
+  shortName: string;
+  inn: string;
+  kpp: string;
+  regionCode: string;
+  dejureAddress: string;
+  factAddress: string;
+}
