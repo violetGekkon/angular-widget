6e5e5308df83b5c9dfebedca39511e8ec9df8a99
sia <violetGekkon@gmail.com>
2023-10-13 14:12:05 +0300

    feat(apps): new remote app added
    npx nx g @nx/angular:remote frontend/flower --host=frontend-angular-widget --style=scss

---
+++ apps/frontend/angular-widget/src/remotes.d.ts
@@ -0,0 +1,1 @@
+declare module 'frontend-flower/Module';
---
+++ apps/frontend/flower/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-frontend-flower-entry',
+  template: `<app-nx-welcome></app-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
